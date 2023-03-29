01c9ff91f3d0684fa5f6fdaaf950860a0e600800
Paul C. Martin <pmartin@gatech.edu>
2023-03-29 14:56:09 -0400

    Fix net.sh

--- net.sh
+++ net.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+# Needs to be run to initialize openvpn in the pod.
+# If running in kubernetes, this can all be applied via a configmap & postStart script.
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200 || true # exit for podman. We will need this to run later
 echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.d/ipv4.conf
 echo "net.ipv4.conf.all.forwarding = 1" >> /etc/sysctl.d/ipv4.conf
-modprobe tun && echo "tun" >> /etc/modules-load.d/tun.conf
+modprobe tun || true # exit for podman. We will need this to run later
+echo "tun" >> /etc/modules-load.d/tun.conf
